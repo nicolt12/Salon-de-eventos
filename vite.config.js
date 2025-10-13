af0dab10a2c5c782f7e902252d02f6d936effc21
nicolt12 <herreranicolasezequiel@hotmail.com>
2025-10-13 17:13:47 -0300

    Elimino base para Vercel, corrige paths de assets

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Salon-de-eventos/',
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:5000'
-    }
-  }
 })
